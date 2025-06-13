48007d54c7a8184ad108c2f1f038cea7cef6f6f4
huuphuc1464 <0306221464@caothang.edu.vn>
2025-06-13 15:49:23 +0700

    thêm seeder 1 số bảng

--- HeThongQuanLyBaiGiang/database/seeders/KhoaSeeder.php
+++ HeThongQuanLyBaiGiang/database/seeders/KhoaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KhoaSeeder extends Seeder
 {
@@ -12,6 +13,35 @@ class KhoaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('khoa')->insert([
+            [
+                'TenKhoa' => 'Công nghệ thông tin',
+                'TrangThai' => 1,
+            ],
+            [
+                'TenKhoa' => 'Cơ khí',
+                'TrangThai' => 1,
+            ],
+            [
+                'TenKhoa' => 'Cơ khí - Động lực',
+                'TrangThai' => 1,
+            ],
+            [
+                'TenKhoa' => 'Điện - Điện tử',
+                'TrangThai' => 1,
+            ],
+            [
+                'TenKhoa' => 'Công nghệ Nhiệt - Lạnh',
+                'TrangThai' => 1,
+            ],
+            [
+                'TenKhoa' => 'Giáo dục đại cương',
+                'TrangThai' => 1,
+            ],
+            [
+                'TenKhoa' => 'Kế toán',
+                'TrangThai' => 1,
+            ]
+        ]);
     }
 }
