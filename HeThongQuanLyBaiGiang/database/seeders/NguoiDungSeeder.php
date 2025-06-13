7833cdd54d6e7c58f64b3b39a90f7831240441a7
huuphuc1464 <0306221464@caothang.edu.vn>
2025-06-13 10:02:05 +0700

    Tạo seeder cho table vai trò, người dùng, sinh viên

--- HeThongQuanLyBaiGiang/database/seeders/NguoiDungSeeder.php
+++ HeThongQuanLyBaiGiang/database/seeders/NguoiDungSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class NguoiDungSeeder extends Seeder
 {
@@ -12,6 +13,91 @@ class NguoiDungSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('nguoi_dung')->insert([
+            [
+                'MaVaiTro' => 1, // admin
+                'TenTaiKhoan' => 'huuphuc1702@gmail.com',
+                'MatKhau' => bcrypt('Admin123@'),
+                'Email' => 'huuphuc1702@gmail.com',
+                'HoTen' => 'Trần Hửu Phúc',
+                'SoDienThoai' => '0901234567',
+                'AnhDaiDien' => null,
+                'DiaChi' => 'Long An',
+                'NgaySinh' => '2004-02-17',
+                'GioiTinh' => 'Nam',
+                'LanDauDangNhap' => true,
+                'TrangThai' => true,
+            ],
+            [
+                'MaVaiTro' => 1, // admin
+                'TenTaiKhoan' => 'duynvdd2424@gmail.com',
+                'MatKhau' => bcrypt('Admin123@'),
+                'Email' => 'duynvdd2424@gmail.com',
+                'HoTen' => 'Ngô Võ Đức Duy',
+                'SoDienThoai' => '0912345678',
+                'AnhDaiDien' => null,
+                'DiaChi' => 'TP.HCM',
+                'NgaySinh' => '2004-01-01',
+                'GioiTinh' => 'Nam',
+                'LanDauDangNhap' => true,
+                'TrangThai' => true,
+            ],
+            [
+                'MaVaiTro' => 2, // teacher
+                'TenTaiKhoan' => '0306221464@caothang.edu.vn',
+                'MatKhau' => bcrypt('Teacher123@'),
+                'Email' => '0306221464@caothang.edu.vn',
+                'HoTen' => 'Trần Hửu Phúc',
+                'SoDienThoai' => '0901234566',
+                'AnhDaiDien' => null,
+                'DiaChi' => 'Long An',
+                'NgaySinh' => '2004-02-17',
+                'GioiTinh' => 'Nam',
+                'LanDauDangNhap' => true,
+                'TrangThai' => true,
+            ],
+            [
+                'MaVaiTro' => 2, // Teacher
+                'TenTaiKhoan' => '0306221407@caothang.edu.vn',
+                'MatKhau' => bcrypt('Teacher123@'),
+                'Email' => '0306221407@caothang.edu.vn',
+                'HoTen' => 'Ngô Võ Đức Duy',
+                'SoDienThoai' => '0912345677',
+                'AnhDaiDien' => null,
+                'DiaChi' => 'TP.HCM',
+                'NgaySinh' => '2004-01-01',
+                'GioiTinh' => 'Nam',
+                'LanDauDangNhap' => true,
+                'TrangThai' => true,
+            ],
+            [
+                'MaVaiTro' => 3, // student
+                'TenTaiKhoan' => 'sv1@caothang.edu.vn',
+                'MatKhau' => bcrypt('Student123@'),
+                'Email' => 'sv1@caothang.edu.vn',
+                'HoTen' => 'Trần Hửu Phúc',
+                'SoDienThoai' => '0901234444',
+                'AnhDaiDien' => null,
+                'DiaChi' => 'Long An',
+                'NgaySinh' => '2004-02-17',
+                'GioiTinh' => 'Nam',
+                'LanDauDangNhap' => true,
+                'TrangThai' => true,
+            ],
+            [
+                'MaVaiTro' => 3, // student
+                'TenTaiKhoan' => 'sv2@caothang.edu.vn',
+                'MatKhau' => bcrypt('Student123@'),
+                'Email' => 'sv2@caothang.edu.vn',
+                'HoTen' => 'Ngô Võ Đức Duy',
+                'SoDienThoai' => '0912348888',
+                'AnhDaiDien' => null,
+                'DiaChi' => 'TP.HCM',
+                'NgaySinh' => '2004-01-01',
+                'GioiTinh' => 'Nam',
+                'LanDauDangNhap' => true,
+                'TrangThai' => true,
+            ]
+        ]);
     }
 }
--- HeThongQuanLyBaiGiang/database/seeders/SinhVienSeeder.php
+++ HeThongQuanLyBaiGiang/database/seeders/SinhVienSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SinhVienSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class SinhVienSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('sinh_vien')->insert([
+            [
+                'MaNguoiDung' => 5,
+                'MSSV' => '0306221464',
+            ],
+            [
+                'MaNguoiDung' => 6,
+                'MSSV' => '0306221453',
+            ],
+        ]);
     }
 }
