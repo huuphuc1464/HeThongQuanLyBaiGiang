b79a931a5944d038967d0fc64688ee7a1b86ce16
ducduy2424 <duynvdd2424@gmail.com>
2025-06-27 23:13:59 +0700

    fix danh sách bài kiểm tra

--- HeThongQuanLyBaiGiang/app/Http/Controllers/SinhVien/KetQuaBaiKiemTraController.php
+++ HeThongQuanLyBaiGiang/app/Http/Controllers/SinhVien/KetQuaBaiKiemTraController.php
@@ -270,7 +270,6 @@ class KetQuaBaiKiemTraController extends Controller
                 $bai->trangThai = 'Đã kết thúc';
             }
         }
-        dd($baiKiemTra);
 
         return view('sinhvien.danhsachbaikiemtra', compact('baiKiemTra'));
     }
