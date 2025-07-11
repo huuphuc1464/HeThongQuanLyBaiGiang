50796c69e6c1499a8f459d36d4108d8a5dfb2615
huuphuc1464 <0306221464@caothang.edu.vn>
2025-07-11 19:12:57 +0700

    update layout sv

--- HeThongQuanLyBaiGiang/resources/views/layouts/sidebarLopHocPhan.blade.php
+++ HeThongQuanLyBaiGiang/resources/views/layouts/sidebarLopHocPhan.blade.php
@@ -14,7 +14,7 @@
         <div class="duongKe my-1 section-title ps-3 nav-link active d-flex align-items-center justify-content-between">
             <div class="d-flex align-items-center gap-2">
                 <img src=" {{ $lopHocPhan->AnhBaiGiang ? asset( $lopHocPhan->AnhBaiGiang) : asset('img/hocphan/default.png') }}" width="30" height="32" alt="Class icon" style="border-radius: 50%; border: 1.5px solid #3a3a3a;" />
-                <span class="text-truncate d-inline-block" style="max-width: 220px; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;" title="{{ $lopHocPhan->TenLopHocPhan }}">
+                <span class="text-truncate d-inline-block" style="max-width: 195px; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;" title="{{ $lopHocPhan->TenLopHocPhan }}">
                     {{ $lopHocPhan->TenLopHocPhan }}
                 </span>
             </div>
